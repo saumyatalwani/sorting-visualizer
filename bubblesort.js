5463c9ed9c5a459a1728a23d5ba36af7922614f9
saumyatalwani <saumyaofficial007@gmail.com>
2023-12-08 15:35:08 +0530

    first commit

---
+++ bubblesort.js
@@ -0,0 +1,48 @@
+const arr = [89, 77, 67, 53, 34];
+
+console.log(arr);
+
+function setPB() {
+    for (let k = 0; k < 5; k++) {
+        pb[k].style.width = arr[k] + "%";
+        pb[k].innerHTML = arr[k];
+    }
+}
+
+function bubbleSortStep(i, j) {
+    if (j < arr.length - i - 1) {
+        if (arr[j] > arr[j + 1]) {
+            const temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+
+        setPB();
+        console.log(arr);
+
+        setTimeout(function () {
+            bubbleSortStep(i, j + 1);
+        }, 1000);
+    } else {
+        setTimeout(function () {
+            bubbleSort(i + 1);
+        }, 1000);
+    }
+}
+
+function bubbleSort(i) {
+    if (i < arr.length - 1) {
+        // Start the inner loop with j = 0
+        bubbleSortStep(i, 0);
+    } else {
+        // Sorting is complete
+        setPB();
+        console.log(arr);
+        document.getElementsByClassName("label")[0].innerHTML="Done!"
+    }
+}
+
+window.onload = function () {
+    pb = document.getElementsByClassName("progress-bar");
+    bubbleSort(0);
+};
